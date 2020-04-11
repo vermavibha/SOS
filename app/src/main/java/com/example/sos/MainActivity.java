eefb8cb9866f93601d8745d785dc6112322ce6a2
vverma14 <vibha.verma.blr@gmail.com>
2020-04-12 00:00:11 +0530

    Added code to access location

--- app/src/main/java/com/example/sos/MainActivity.java
+++ app/src/main/java/com/example/sos/MainActivity.java
@@ -1,23 +1,63 @@
 package com.example.sos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickbtn(View view){
+    private FusedLocationProviderClient client;
+
+    /*public void clickbtn(View view){
         String toastText = "Be strong, we have send your MAYDAY request.";
         Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        Button button = findViewById(R.id.sos);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location!=null){
+                            TextView textview = findViewById(R.id.location);
+                            textview.setText(location.toString());
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 }
